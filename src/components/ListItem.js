dae6b7f61fde37116fbf7c1a2a76da10fdcca42e
BamSquid <lpfan333@gmail.com>
2021-07-09 08:11:09 -0400

    add function to update total percent

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,6 +1,29 @@
 import React from 'react'
 
 class ListItem extends React.Component {
+
+    updatePercentage = (e) => {
+        const clicked = e.target;
+        var parent = clicked.parentElement;
+        const isClicked = parent.getAttribute('data-clicked');
+        const percent = parseInt(parent.getAttribute('data-percent'));
+        const border = isClicked === 'true' ? 'solid black 1px' : 'solid white 1px';
+        const updateClicked = isClicked === 'false' ? 'true' : 'false'
+        var children = parent.children;
+
+        for (var i = 0; i < children.length; i++) {
+            children[i].style.border = border;
+        }
+        parent.setAttribute('data-clicked', updateClicked);
+
+        var total = document.getElementById('total-percent');
+        var totalPercent = parseInt(total.getAttribute('data-percent'));
+        totalPercent += updateClicked === 'true' ? percent : percent * -1;
+        total.setAttribute('data-percent', totalPercent);
+        total.textContent = 'Completion: ' + totalPercent + '%';
+
+    }
+
     render() {
         var details = this.props.details;
         var headings = this.props.headings;
@@ -12,8 +35,7 @@ class ListItem extends React.Component {
             row.push(<td>{data}</td>)
         }
         return (
-            <tr>
-                <input type='checkbox' />
+            <tr data-percent={details.percentage} data-clicked={'false'} onClick={this.updatePercentage}>
                 {row}
             </tr>
         )
