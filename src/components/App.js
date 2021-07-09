af689ca675aa23dc2069452e7d58098a54b34071
BamSquid <lpfan333@gmail.com>
2021-07-09 08:10:22 -0400

    add fixed footer for total percent complete

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,14 @@ function App() {
   console.log(checklist);
   var sections = [];
   for (var i = 0; i < checklist.length; i++) {
-    sections.push(<Section name={checklist[i].section} items={checklist[i].items} />);
+    sections.push(<Section key={'section' + i} name={checklist[i].section} items={checklist[i].items} />);
   }
   return (
     <div className="App">
       <header className="App-header">
         {sections}
       </header>
+      <div id='total-percent' className='footer' data-percent='0'>Completion: 0%</div>
     </div>
   );
 }
