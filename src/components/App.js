98fd51776ba5e712867e076ddd42080a281bb9eb
BamSquid <lpfan333@gmail.com>
2021-07-12 07:14:49 -0400

    add charm notch counter and total percent vars

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,41 @@
-import './css/App.css';
-import Section from './Section.js'
-import checklist from '../checklist.json'
+import "./css/App.css";
+import Section from "./Section.js";
+import checklist from "../checklist.json";
 
 function App() {
-  console.log(checklist);
   var sections = [];
   for (var i = 0; i < checklist.length; i++) {
-    sections.push(<Section key={'section' + i} name={checklist[i].section} items={checklist[i].items} />);
+    sections.push(
+      <Section
+        key={"section" + i}
+        name={checklist[i].section}
+        items={checklist[i].items}
+      />
+    );
   }
   return (
     <div className="App">
-      <header className="App-header">
-        {sections}
-      </header>
-      <div id='total-percent' className='footer' data-percent='0'>Completion: 0%</div>
+      <header className="App-header">{sections}</header>
+      <div className="footer">
+        <div
+          id="total-percent"
+          data-percent="0"
+          data-masks="0"
+          data-mask-percent="0"
+          data-soul="0"
+          data-soul-percent="0"
+          style={{'padding-inline-start': '15px', 'padding-inline-end': '15px'}}
+        >
+          Completion: 0%
+        </div>
+        <div
+          id="total-notches"
+          data-notches="3"
+          style={{'padding-inline-start': '40px', 'padding-inline-end': '40px'}}
+        >
+          Charm Notches: 3
+        </div>
+      </div>
     </div>
   );
 }
