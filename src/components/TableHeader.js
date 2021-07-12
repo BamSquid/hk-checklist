9309383fa0f099e3ce46c9a259063bafc12eb97a
BamSquid <lpfan333@gmail.com>
2021-07-12 07:15:10 -0400

    update TableHeader format I guess

--- src/components/TableHeader.js
+++ src/components/TableHeader.js
@@ -1,20 +1,18 @@
-import React from 'react'
+import React from "react";
 
 class TableHeader extends React.Component {
-    render() {
-        var headings = this.props.headings;
-        var rows = [];
-        for (var i = 0; i < headings.length; i++) {
-            rows.push(<th>{headings[i]}</th>)
-        }
-        return (
-            <thead>
-            <tr>
-                {rows}
-            </tr>
-        </thead>
-        )
+  render() {
+    var headings = this.props.headings;
+    var rows = [];
+    for (var i = 0; i < headings.length; i++) {
+      rows.push(<th>{headings[i]}</th>);
     }
+    return (
+      <thead>
+        <tr>{rows}</tr>
+      </thead>
+    );
+  }
 }
 
-export default TableHeader
+export default TableHeader;
