cbe64e3e56a35245b862db64cd3d658d3f9a7e61
BamSquid <lpfan333@gmail.com>
2021-07-19 14:31:43 -0400

    Update ListItem.js
    Properly uses states. Also fixed the weird issue where the state was backwards from what happened (basically before, it did the calculations based on what the state was before it was clicked. Now it does it afterwards, which is the proper way and is a lot less confusing to follow).

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -3,37 +3,31 @@ import React from "react";
 class ListItem extends React.Component {
   state = {
     checked: false
-  }
+  };
 
   updatePercentage = (e) => {
-    this.setState({checked: !this.state.checked})
-
     const clicked = e.target;
-    var tr = clicked.tagName.toLowerCase() === 'tr' ? clicked : clicked.parentElement;
-    const isChecked = this.state.checked;
+    var tr = clicked.tagName.toLowerCase() === "tr" ? clicked : clicked.parentElement;
+    const isChecked = !this.state.checked;
 
     const percent = parseInt(tr.getAttribute("data-percent"), 10);
-    const border = isChecked ? "solid white 1px" : "solid black 1px";
-    const background = isChecked ? "#282c34" : "white";
-    const color = isChecked ? "white" : "black";
 
     var children = tr.children;
     for (var i = 0; i < children.length; i++) {
-      children[i].style.border = border;
-      children[i].style.background = background;
-      children[i].style.color = color;
+      if (isChecked) { children[i].classList.add('checked'); }
+      else { children[i].classList.remove('checked'); }
     }
 
     var total = document.getElementById("total-percent");
     var totalPercent = parseInt(total.getAttribute("data-percent"), 10);
     if (!isNaN(percent)) {
-      totalPercent += !isChecked ? percent : percent * -1;
+      totalPercent += isChecked ? percent : percent * -1;
     }
     else {
       var section = this.props.section;
       if (section === "masks") {
         var num_masks = parseInt(total.getAttribute("data-masks"), 10);
-        num_masks += !isChecked ? 1 : -1;
+        num_masks += isChecked ? 1 : -1;
         total.setAttribute("data-masks", num_masks);
 
         const maskPercent = total.getAttribute("data-mask-percent");
@@ -49,7 +43,7 @@ class ListItem extends React.Component {
       }
       else if (section === "soul vessels") {
         var num_soul = parseInt(total.getAttribute("data-soul"), 10);
-        num_soul += !isChecked ? 1 : -1;
+        num_soul += isChecked ? 1 : -1;
         total.setAttribute("data-soul", num_soul);
 
         const soulPercent = total.getAttribute("data-soul-percent");
@@ -64,31 +58,27 @@ class ListItem extends React.Component {
         }
       }
       else if (section === "charm notches") {
-        var notchCounter = document.getElementById('total-notches');
-        var numNotches = parseInt(notchCounter.getAttribute('data-notches'), 10);
-        numNotches += !isChecked ? 1 : -1;
-        notchCounter.setAttribute('data-notches', numNotches);
+        var notchCounter = document.getElementById("total-notches");
+        var numNotches = parseInt(notchCounter.getAttribute("data-notches"), 10);
+        numNotches += isChecked ? 1 : -1;
+        notchCounter.setAttribute("data-notches", numNotches);
         notchCounter.textContent = "Charm Notches: " + numNotches;
 
-        if (numNotches === 11) {
-          notchCounter.classList.add('complete');
-        }
-        else {
-          notchCounter.classList.remove('complete');
-        }
+        if (numNotches === 11) { notchCounter.classList.add("complete"); }
+        else { notchCounter.classList.remove("complete"); }
       }
     }
 
     total.setAttribute("data-percent", totalPercent);
     total.textContent = "Completion: " + totalPercent + "%";
-    if (totalPercent === 112) {
-      total.classList.add('complete');
-    }
-    else {
-        if (total.classList.contains('complete')) {
-            total.classList.remove('complete');
-        }
-    }
+    if (totalPercent === 112) { total.classList.add("complete"); }
+    else { total.classList.remove("complete"); }
+    
+    this.setState({ checked: isChecked });
+
+    var data = {};
+    data[this.props.id] = isChecked;
+    this.props.onChange(data);
   };
 
   render() {
@@ -103,10 +93,7 @@ class ListItem extends React.Component {
     }
 
     return (
-      <tr
-        data-percent={details.percentage}
-        onClick={this.updatePercentage}
-      >
+      <tr id={this.props.id} data-percent={details.percentage} onClick={this.updatePercentage}>
         {row}
       </tr>
     );
